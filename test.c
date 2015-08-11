22d26f626d29c3ff400ea668d01014b9a496932e
David Grayson <davidegrayson@gmail.com>
2015-08-11 09:10:15 -0700

    Made it not overflow when the result argument points to a boolean.

--- test.c
+++ test.c
@@ -1,8 +1,23 @@
 /** Tests for a compiler's implementation of __builtin_add_overflow,
  * __builtin_sub_overflow, and __builtin_mul_overflow. **/
 
+/* To make sure we cover all the important cases, we should list out
+ * the dimensions and then methodically explore many combinations of
+ * those dimensions.  The dimensions are:
+ *
+ * 1) Operation: add, sub, mul
+ * 2) Types of the two inputs
+ * 3) Type of the output
+ * 4) Values of the inputs
+ *
+ * Integer types should be ordered by their maximum value, like this:
+ *   bool, int8_t, uint8_t, int16_t, uint16_t, ... uint128_t
+ *
+ */
+
 #include <stdio.h>
 #include <stdint.h>
+#include <stdbool.h>
 #include <stdarg.h>
 #include <limits.h>
 
@@ -48,6 +63,11 @@ static void test_errors()
 
 static void test_add()
 {
+  // bool + bool -> bool
+  TEST_ADD((bool)0,            (bool)0,            (bool)0,            0);
+  TEST_ADD((bool)0,            (bool)1,            (bool)1,            0);
+  TEST_ADD((bool)1,            (bool)1,            (bool)1,            1);
+
   TEST_ADD((int32_t)1,         (int32_t)2,         (int32_t)3,         0);
   TEST_ADD((int32_t)INT_MAX,   (int32_t)0,         (int32_t)INT_MAX,   0);
   TEST_ADD((int32_t)INT_MAX,   (int32_t)1,         (int32_t)INT_MIN,   1);
@@ -55,6 +75,8 @@ static void test_add()
   TEST_ADD((uint32_t)1,        (uint32_t)2,        (uint32_t)3,         0);
   TEST_ADD((uint32_t)UINT_MAX, (uint32_t)0,        (uint32_t)UINT_MAX,  0);
   TEST_ADD((uint32_t)UINT_MAX, (uint32_t)1,        (uint32_t)0,         1);
+
+  //TEST_ADD((uint32_t)1,        (int32_t)2,         (uint32_t)3,         0);
 }
 
 int main()
