057b1ae9951c8242c4f948046c224334aac9f3a6
David Grayson <davidegrayson@gmail.com>
2015-08-16 21:38:46 -0700

    Moved development to Linux.

--- get_patch.sh
+++ get_patch.sh
@@ -1,4 +1,5 @@
 {
-  cd /d/builds/mingw-packages/mingw-w64-clang-svn/src/llvm/tools/clang
+  cd ~/llvm_dev/llvm/tools/clang
+  # cd /d/builds/mingw-packages/mingw-w64-clang-svn/src/llvm/tools/clang
   git diff -u
 } > clang.patch
