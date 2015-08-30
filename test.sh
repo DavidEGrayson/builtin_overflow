f4c9bcf8045a1287313b906959dc96f80ca4f282
David Grayson <davidegrayson@gmail.com>
2015-08-30 13:37:14 -0700

    Added a test for builtin overflow compilation errors, but I am not sure how to keep that test to less than 80 characters per line.

--- test.sh
+++ test.sh
@@ -1,2 +1,6 @@
-~/llvm_dev/build/bin/clang -cc1 -internal-isystem ~/llvm_dev/build/lib/clang/3.8.0/include -nostdsysteminc -triple i686-unknown-unknown -emit-llvm -x c ~/llvm_dev/llvm/tools/clang/test/CodeGen/builtins-overflow.c -o test.ll
-cat test.ll | ~/llvm_dev/build/bin/FileCheck ~/llvm_dev/llvm/tools/clang/test/CodeGen/builtins-overflow.c
+export PATH=~/llvm_dev/build/bin:$PATH
+
+clang -cc1 -internal-isystem ~/llvm_dev/build/lib/clang/3.8.0/include -nostdsysteminc -triple i686-unknown-unknown -emit-llvm -x c ~/llvm_dev/llvm/tools/clang/test/CodeGen/builtins-overflow.c -o test.ll
+cat test.ll | FileCheck ~/llvm_dev/llvm/tools/clang/test/CodeGen/builtins-overflow.c
+
+clang -cc1 -Wall -Wno-unused -Werror -fsyntax-only -verify ~/llvm_dev/llvm/tools/clang/test/CodeGen/builtins-overflow-error.c
