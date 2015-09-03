4c0eb1bb0ad3cadb3a69eef1ed50ac4112399dbd
David Grayson <davidegrayson@gmail.com>
2015-09-02 23:32:06 -0700

    update test.sh to also run the test program test.c

--- test.sh
+++ test.sh
@@ -1,6 +1,9 @@
 export PATH=~/llvm_dev/build/bin:$PATH
+S=~/llvm_dev/llvm-svn/tools/clang
 
-clang -cc1 -internal-isystem ~/llvm_dev/build/lib/clang/3.8.0/include -nostdsysteminc -triple i686-unknown-unknown -emit-llvm -x c ~/llvm_dev/llvm/tools/clang/test/CodeGen/builtins-overflow.c -o test.ll
-cat test.ll | FileCheck ~/llvm_dev/llvm/tools/clang/test/CodeGen/builtins-overflow.c
+clang -cc1 -internal-isystem ~/llvm_dev/build/lib/clang/3.8.0/include -nostdsysteminc -triple i686-unknown-unknown -emit-llvm -x c $S/test/CodeGen/builtins-overflow.c -o test.ll
+cat test.ll | FileCheck $S/test/CodeGen/builtins-overflow.c
 
-clang -cc1 -Wall -Wno-unused -Werror -fsyntax-only -verify ~/llvm_dev/llvm/tools/clang/test/CodeGen/builtins-overflow-error.c
+clang -cc1 -Wall -Wno-unused -Werror -fsyntax-only -verify $S/test/CodeGen/builtins-overflow-error.c
+
+clang test.c -o test && ./test
